105d3cb1fd9942189c92a4ea1e67b156c34a6c66
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2023-08-03 22:10:31 +0700

    Update LED4.ino

--- Arduino nano code/LED4.ino
+++ Arduino nano code/LED4.ino
@@ -3,7 +3,7 @@
 #define NUM_LEDS 50 //specify number of LED
 
 CRGB leds[NUM_LEDS];
-
+int pass = 0;
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, RGB>(leds, NUM_LEDS);
   FastLED.setMaxPowerInVoltsAndMilliamps(5,500);
@@ -13,19 +13,20 @@ void setup() {
   int r = 0;
   int g = 0;
   int b = 0;
+  
 }
 
-void loop() {
+void loop() { 
    String incoming = Serial.readString();
    Serial.println(incoming);
-   // Get serial data
-   // Check if serial data equal any color name
+   
    if(incoming == "blue"){
       for(int j=0; j<NUM_LEDS; j++){
         leds[j] = CRGB(0,0,255);
         FastLED.show();
         delay(1.5);
       }
+      pass = 0;
    }
 
    if(incoming == "green"){
@@ -34,6 +35,7 @@ void loop() {
         FastLED.show();
         delay(1.5);
       }
+      pass = 0;
    }
 
    if(incoming == "red"){
@@ -42,6 +44,7 @@ void loop() {
         FastLED.show();
         delay(1.5);
       }
+      pass = 0;
    }
 
    if(incoming == "off"){
@@ -50,6 +53,7 @@ void loop() {
         FastLED.show();
         delay(1.5);
       }
+      pass = 0;
    }
 
    if(incoming == "white"){
@@ -58,6 +62,25 @@ void loop() {
         FastLED.show();
         delay(1.5);
       }
+      pass = 0;
+   }
+
+   if(incoming == "purple"){
+      for(int j=0; j<NUM_LEDS; j++){
+        leds[j] = CRGB::Purple;
+        FastLED.show();
+        delay(1.5);
+      }
+      pass = 0;
+   }
+
+   if(incoming == "pink"){
+      for(int j=0; j<NUM_LEDS; j++){
+        leds[j] = CRGB::DeepPink;
+        FastLED.show();
+        delay(1.5);
+      }
+      pass = 0;
    }
 
    if(incoming == "yellow"){
@@ -66,9 +89,62 @@ void loop() {
         FastLED.show();
         delay(1.5);
       }
+      pass = 0;
+   }
+
+   if(incoming == "orange"){
+      for(int j=0; j<NUM_LEDS; j++){
+        leds[j] = CRGB::Orange;
+        FastLED.show();
+        delay(1.5);
+      }
+      pass = 0;
+   }
+   
+   if(incoming == "rainbow"){
+      rainbow_wave(1000, 10);                                      // Speed, delta hue values.
+      FastLED.show();
+      pass = 0;
+   }
+   
+   if(incoming == "whiteflow" && pass == 0){
+      light_flowing();
+      FastLED.show();
+      pass = 1;
+   }
+   if(incoming == "" && pass == 1){
+      light_flowing();
+      FastLED.show();
+   }
+} 
+
+
+void rainbow_wave(uint8_t thisSpeed, uint8_t deltaHue) {     // The fill_rainbow call doesn't support brightness levels.
+ 
+// uint8_t thisHue = beatsin8(thisSpeed,0,255);                // A simple rainbow wave.
+ uint8_t thisHue = beat8(thisSpeed,255);                     // A simple rainbow march.
+  
+ fill_rainbow(leds, NUM_LEDS, thisHue, deltaHue);            // Use FastLED's fill_rainbow routine.
+ 
+} // rainbow_wave()
+
+void light_flowing(){
+   // Move a single white led 
+    for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+      // Turn our current led on to white, then show the leds
+      leds[whiteLed] = CRGB::White;
+
+      // Show the leds (only one of which is set to white, from above)
+      FastLED.show();
+
+      // Wait a little bit
+      delay(30);
+
+      // Turn our current led back to black for the next loop around
+      leds[whiteLed] = CRGB::Black;
    }
 }
-
-
-
-
+  
+  
+    
+ 
